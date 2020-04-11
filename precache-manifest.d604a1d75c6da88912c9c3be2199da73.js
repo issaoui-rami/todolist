bd8eb2e9cb1964e10acf5d497c42d352b34cb634
issaoui-rami <ramiissaoui55@gmail.com>
2020-04-11 21:36:37 +0200

    Updates

---
+++ precache-manifest.d604a1d75c6da88912c9c3be2199da73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6158c9f096a985a1915757164ffcb8e",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a5cb14243cdccb0a8f20",
+    "url": "/todolist/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a5cb14243cdccb0a8f20",
+    "url": "/todolist/static/js/2.dea7268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.dea7268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf7d8b80c6247ed55f6e",
+    "url": "/todolist/static/js/main.4591ca11.chunk.js"
+  },
+  {
+    "revision": "48feb065b8730ec92d38",
+    "url": "/todolist/static/js/runtime-main.055d686a.js"
+  }
+]);
